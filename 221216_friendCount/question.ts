bb2aca78a37dc96165ae9d9ffc0efd97b49e4bde
itisdoyoonkim <dkim.steve@gmail.com>
2022-12-17 18:05:17 -0800

    findMostPopular solved

--- 221216_friendCount/question.ts
+++ 221216_friendCount/question.ts
@@ -1,226 +1,223 @@
 const people = [
     {
-        "guid": "91d8dd08-e09e-4415-9d0a-73d8a4bb2a61",
-        "name": "Cleveland Herman",
-        "gender": "male",
-        "friends": [
-            "807d37f5-d26e-45f3-b4d5-38075eb0408e",
-            "bffecbe8-087e-416d-9036-4452dbef3a02"
-        ]
+        guid: "91d8dd08-e09e-4415-9d0a-73d8a4bb2a61",
+        name: "Cleveland Herman",
+        gender: "male",
+        friends: ["807d37f5-d26e-45f3-b4d5-38075eb0408e", "bffecbe8-087e-416d-9036-4452dbef3a02"],
     },
     {
-        "guid": "1d936ca0-861f-44f9-824d-07c50ba6f99c",
-        "name": "Welch Kelley",
-        "gender": "male",
-        "friends": [
+        guid: "1d936ca0-861f-44f9-824d-07c50ba6f99c",
+        name: "Welch Kelley",
+        gender: "male",
+        friends: [
             "5374e43f-4107-4263-baa0-cc4e334b5ba2",
             "39014445-18a3-45aa-8fa9-20240badc64b",
-            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41"
-        ]
+            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
+        ],
     },
     {
-        "guid": "5374e43f-4107-4263-baa0-cc4e334b5ba2",
-        "name": "Flynn Wright",
-        "gender": "male",
-        "friends": [
+        guid: "5374e43f-4107-4263-baa0-cc4e334b5ba2",
+        name: "Flynn Wright",
+        gender: "male",
+        friends: [
             "3c94302d-76ad-45f7-a280-6484a874194e",
             "2d859341-6276-4d41-932d-06d40de9fe01",
-            "807d37f5-d26e-45f3-b4d5-38075eb0408e"
-        ]
+            "807d37f5-d26e-45f3-b4d5-38075eb0408e",
+        ],
     },
     {
-        "guid": "bffecbe8-087e-416d-9036-4452dbef3a02",
-        "name": "Pitts Harper",
-        "gender": "male",
-        "friends": [
+        guid: "bffecbe8-087e-416d-9036-4452dbef3a02",
+        name: "Pitts Harper",
+        gender: "male",
+        friends: [
             "3c94302d-76ad-45f7-a280-6484a874194e",
             "ebe531ab-c95a-45be-966a-c436a3a6bd28",
             "91d8dd08-e09e-4415-9d0a-73d8a4bb2a61",
             "83862813-e71f-494a-bb27-dfe4b14fcb8c",
-            "c4fefca6-1f93-44c7-b625-fcbcff89ab5a"
-        ]
+            "c4fefca6-1f93-44c7-b625-fcbcff89ab5a",
+        ],
     },
     {
-        "guid": "807d37f5-d26e-45f3-b4d5-38075eb0408e",
-        "name": "Raquel Wilcox",
-        "gender": "female",
-        "friends": [
-            "ebe531ab-c95a-45be-966a-c436a3a6bd28",
-            "c4fefca6-1f93-44c7-b625-fcbcff89ab5a"
-        ]
+        guid: "807d37f5-d26e-45f3-b4d5-38075eb0408e",
+        name: "Raquel Wilcox",
+        gender: "female",
+        friends: ["ebe531ab-c95a-45be-966a-c436a3a6bd28", "c4fefca6-1f93-44c7-b625-fcbcff89ab5a"],
     },
     {
-        "guid": "5828792c-875e-4be5-9e8a-bb814ada2822",
-        "name": "Rosanna Cote",
-        "gender": "female",
-        "friends": [
-            "807d37f5-d26e-45f3-b4d5-38075eb0408e",
-            "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6"
-        ]
+        guid: "5828792c-875e-4be5-9e8a-bb814ada2822",
+        name: "Rosanna Cote",
+        gender: "female",
+        friends: ["807d37f5-d26e-45f3-b4d5-38075eb0408e", "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6"],
     },
     {
-        "guid": "f79b5117-bcb8-482d-ba96-22620277abf9",
-        "name": "Russell Irwin",
-        "gender": "male",
-        "friends": [
+        guid: "f79b5117-bcb8-482d-ba96-22620277abf9",
+        name: "Russell Irwin",
+        gender: "male",
+        friends: [
             "91d8dd08-e09e-4415-9d0a-73d8a4bb2a61",
             "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
-            "807d37f5-d26e-45f3-b4d5-38075eb0408e"
-        ]
+            "807d37f5-d26e-45f3-b4d5-38075eb0408e",
+        ],
     },
     {
-        "guid": "fa63b056-b572-4fdd-be55-3550dc870599",
-        "name": "Scott Mccoy",
-        "gender": "male",
-        "friends": [
+        guid: "fa63b056-b572-4fdd-be55-3550dc870599",
+        name: "Scott Mccoy",
+        gender: "male",
+        friends: [
             "83862813-e71f-494a-bb27-dfe4b14fcb8c",
             "ebe531ab-c95a-45be-966a-c436a3a6bd28",
             "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
             "bffecbe8-087e-416d-9036-4452dbef3a02",
-            "1d936ca0-861f-44f9-824d-07c50ba6f99c"
-        ]
+            "1d936ca0-861f-44f9-824d-07c50ba6f99c",
+        ],
     },
     {
-        "guid": "e1aae357-b9a5-41e0-bb6e-08692d93ef44",
-        "name": "Patsy Green",
-        "gender": "female",
-        "friends": [
-            "5374e43f-4107-4263-baa0-cc4e334b5ba2",
-            "807d37f5-d26e-45f3-b4d5-38075eb0408e"
-        ]
+        guid: "e1aae357-b9a5-41e0-bb6e-08692d93ef44",
+        name: "Patsy Green",
+        gender: "female",
+        friends: ["5374e43f-4107-4263-baa0-cc4e334b5ba2", "807d37f5-d26e-45f3-b4d5-38075eb0408e"],
     },
     {
-        "guid": "2d859341-6276-4d41-932d-06d40de9fe01",
-        "name": "Gwen Cooper",
-        "gender": "female",
-        "friends": [
+        guid: "2d859341-6276-4d41-932d-06d40de9fe01",
+        name: "Gwen Cooper",
+        gender: "female",
+        friends: [
             "ebe531ab-c95a-45be-966a-c436a3a6bd28",
             "5828792c-875e-4be5-9e8a-bb814ada2822",
-            "83862813-e71f-494a-bb27-dfe4b14fcb8c"
-        ]
+            "83862813-e71f-494a-bb27-dfe4b14fcb8c",
+        ],
     },
     {
-        "guid": "b40bcb22-5ab7-4984-828a-28ca9c8ee1eb",
-        "name": "Weber Holt",
-        "gender": "male",
-        "friends": [
+        guid: "b40bcb22-5ab7-4984-828a-28ca9c8ee1eb",
+        name: "Weber Holt",
+        gender: "male",
+        friends: [
             "ea396a38-9fb8-485d-9c7d-103303c9a00a",
             "3c94302d-76ad-45f7-a280-6484a874194e",
             "2d859341-6276-4d41-932d-06d40de9fe01",
             "1d936ca0-861f-44f9-824d-07c50ba6f99c",
-            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41"
-        ]
+            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
+        ],
     },
     {
-        "guid": "83862813-e71f-494a-bb27-dfe4b14fcb8c",
-        "name": "Alexander Pugh",
-        "gender": "male",
-        "friends": [
+        guid: "83862813-e71f-494a-bb27-dfe4b14fcb8c",
+        name: "Alexander Pugh",
+        gender: "male",
+        friends: [
             "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6",
             "39014445-18a3-45aa-8fa9-20240badc64b",
             "b40bcb22-5ab7-4984-828a-28ca9c8ee1eb",
             "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
-            "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94"
-        ]
+            "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
+        ],
     },
     {
-        "guid": "ea396a38-9fb8-485d-9c7d-103303c9a00a",
-        "name": "Weiss Blevins",
-        "gender": "male",
-        "friends": [
-            "fa63b056-b572-4fdd-be55-3550dc870599",
-            "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6"
-        ]
+        guid: "ea396a38-9fb8-485d-9c7d-103303c9a00a",
+        name: "Weiss Blevins",
+        gender: "male",
+        friends: ["fa63b056-b572-4fdd-be55-3550dc870599", "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6"],
     },
     {
-        "guid": "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
-        "name": "Nichols Allison",
-        "gender": "male",
-        "friends": [
+        guid: "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
+        name: "Nichols Allison",
+        gender: "male",
+        friends: [
             "ea396a38-9fb8-485d-9c7d-103303c9a00a",
             "39014445-18a3-45aa-8fa9-20240badc64b",
-            "5374e43f-4107-4263-baa0-cc4e334b5ba2"
-        ]
+            "5374e43f-4107-4263-baa0-cc4e334b5ba2",
+        ],
     },
     {
-        "guid": "3c94302d-76ad-45f7-a280-6484a874194e",
-        "name": "Peck Walsh",
-        "gender": "male",
-        "friends": [
+        guid: "3c94302d-76ad-45f7-a280-6484a874194e",
+        name: "Peck Walsh",
+        gender: "male",
+        friends: [
             "bffecbe8-087e-416d-9036-4452dbef3a02",
             "f79b5117-bcb8-482d-ba96-22620277abf9",
             "39014445-18a3-45aa-8fa9-20240badc64b",
             "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
-            "e1aae357-b9a5-41e0-bb6e-08692d93ef44"
-        ]
+            "e1aae357-b9a5-41e0-bb6e-08692d93ef44",
+        ],
     },
     {
-        "guid": "c4fefca6-1f93-44c7-b625-fcbcff89ab5a",
-        "name": "Tia Shepherd",
-        "gender": "female",
-        "friends": [
+        guid: "c4fefca6-1f93-44c7-b625-fcbcff89ab5a",
+        name: "Tia Shepherd",
+        gender: "female",
+        friends: [
             "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
             "1d936ca0-861f-44f9-824d-07c50ba6f99c",
             "5374e43f-4107-4263-baa0-cc4e334b5ba2",
-            "39014445-18a3-45aa-8fa9-20240badc64b"
-        ]
+            "39014445-18a3-45aa-8fa9-20240badc64b",
+        ],
     },
     {
-        "guid": "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6",
-        "name": "Bryan Hurst",
-        "gender": "male",
-        "friends": [
+        guid: "bb95861e-fc8d-4722-b0c8-ba9cd12ec7e6",
+        name: "Bryan Hurst",
+        gender: "male",
+        friends: [
             "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
             "e1aae357-b9a5-41e0-bb6e-08692d93ef44",
-            "39014445-18a3-45aa-8fa9-20240badc64b"
-        ]
+            "39014445-18a3-45aa-8fa9-20240badc64b",
+        ],
     },
     {
-        "guid": "ebe531ab-c95a-45be-966a-c436a3a6bd28",
-        "name": "Phelps Terrell",
-        "gender": "male",
-        "friends": [
+        guid: "ebe531ab-c95a-45be-966a-c436a3a6bd28",
+        name: "Phelps Terrell",
+        gender: "male",
+        friends: [
             "b40bcb22-5ab7-4984-828a-28ca9c8ee1eb",
             "2d859341-6276-4d41-932d-06d40de9fe01",
             "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
             "ea396a38-9fb8-485d-9c7d-103303c9a00a",
-            "3c94302d-76ad-45f7-a280-6484a874194e"
-        ]
+            "3c94302d-76ad-45f7-a280-6484a874194e",
+        ],
     },
     {
-        "guid": "39014445-18a3-45aa-8fa9-20240badc64b",
-        "name": "Madeline Calderon",
-        "gender": "female",
-        "friends": [
+        guid: "39014445-18a3-45aa-8fa9-20240badc64b",
+        name: "Madeline Calderon",
+        gender: "female",
+        friends: [
             "91d8dd08-e09e-4415-9d0a-73d8a4bb2a61",
             "2d859341-6276-4d41-932d-06d40de9fe01",
             "f79b5117-bcb8-482d-ba96-22620277abf9",
-            "ea396a38-9fb8-485d-9c7d-103303c9a00a"
-        ]
+            "ea396a38-9fb8-485d-9c7d-103303c9a00a",
+        ],
     },
     {
-        "guid": "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
-        "name": "May Walter",
-        "gender": "female",
-        "friends": [
+        guid: "1ae47cd9-7a9e-4e09-8483-caa1f71f1f94",
+        name: "May Walter",
+        gender: "female",
+        friends: [
             "5374e43f-4107-4263-baa0-cc4e334b5ba2",
             "83862813-e71f-494a-bb27-dfe4b14fcb8c",
             "fa63b056-b572-4fdd-be55-3550dc870599",
-            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41"
-        ]
-    }
+            "c1e2bd01-d11f-4632-9d93-afb0fd5acd41",
+        ],
+    },
 ];
 
 // A GUID in the friends property is the GUID of a person in the list.
 // Write a function to return the name of a person with the most friends.
 // If there are more than 1 person with the highest friend count, return any one of them.
-function findMostPopular() {
+function findMostPopular(people) {
+    let friendCount = 0;
+    let personWithMostFriends;
 
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+
+        if (person.friends.length > friendCount) {
+            friendCount = person.friends.length;
+            personWithMostFriends = person;
+        }
+    }
+
+    return personWithMostFriends;
 }
+
+console.log(findMostPopular(people));
 
 // A GUID in the friends property is the GUID of a person in the list.
 // Write a function to return the name of a person with the most male friends
 // If there are more than 1 person with the highest male friend count, return any one of them.
-function findPersonWithMostMaleFriends() {
-
-}
+function findPersonWithMostMaleFriends() {}
