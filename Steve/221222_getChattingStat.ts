c09f29b60b10b1d37a0d4fed75a062654b9cd74c
itisdoyoonkim <dkim.steve@gmail.com>
2023-01-04 14:20:31 -0800

    question 1 & 2 revised

--- Steve/221222_getChattingStat.ts
+++ Steve/221222_getChattingStat.ts
@@ -1,5 +1,4 @@
-import { input221222 } from "../Questions/221222_getChattingStat";
-
+import { input221222, Input221222, Output221222 } from "../Questions/221222_getChattingStat";
 const Output221222 = {
     mostTalkativePerson: "",
     mostTalkativeInRoom: {},
@@ -11,20 +10,19 @@ const Output221222 = {
 // Find who is the most talkative person in each room
 // Find who is participating in most chat room
 // Time Complexity expectation: O(n)
-function getChattingStat(input221222) {
+function getChattingStat(input: Input221222): Output221222 {
     // 1. Find the most talkative person overall
-    const mostTalkativePersonOverallMap = new Map();
-    input221222.forEach(({ senderId }) => {
-        if (!mostTalkativePersonOverallMap[senderId]) mostTalkativePersonOverallMap[senderId] = 1;
-
-        mostTalkativePersonOverallMap[senderId] += 1;
+    const mostTalkativePersonOverall = {};
+    input.forEach(({ senderId }) => {
+        if (!mostTalkativePersonOverall[senderId]) mostTalkativePersonOverall[senderId] = 0;
+        mostTalkativePersonOverall[senderId] += 1;
     });
 
     let overallMsgCount = 0;
     let mostTalkativePerson;
-    for (let person in mostTalkativePersonOverallMap) {
-        let currentPerson = person;
-        let currentPersonMsgCount = mostTalkativePersonOverallMap[currentPerson];
+    for (const person in mostTalkativePersonOverall) {
+        const currentPerson = person;
+        const currentPersonMsgCount = mostTalkativePersonOverall[currentPerson];
         if (currentPersonMsgCount >= overallMsgCount) {
             overallMsgCount = currentPersonMsgCount;
             mostTalkativePerson = currentPerson;
@@ -33,22 +31,19 @@ function getChattingStat(input221222) {
     Output221222.mostTalkativePerson = mostTalkativePerson;
 
     // 2. Find the most talkative person in each room
-    const msgsPerChatRoomMap = new Map();
-    input221222.forEach(({ chatRoomId, senderId }) => {
-        if (!msgsPerChatRoomMap[chatRoomId]) msgsPerChatRoomMap[chatRoomId] = new Map();
-
-        if (!msgsPerChatRoomMap[chatRoomId][senderId]) {
-            msgsPerChatRoomMap[chatRoomId][senderId] = 1;
-        }
-        msgsPerChatRoomMap[chatRoomId][senderId] += 1;
+    const msgsPerChatRoom = {};
+    input.forEach(({ chatRoomId, senderId }) => {
+        if (!msgsPerChatRoom[chatRoomId]) msgsPerChatRoom[chatRoomId] = {};
+        if (!msgsPerChatRoom[chatRoomId][senderId]) msgsPerChatRoom[chatRoomId][senderId] = 0;
+        msgsPerChatRoom[chatRoomId][senderId] += 1;
     });
 
-    for (let room in msgsPerChatRoomMap) {
+    for (const room in msgsPerChatRoom) {
         let msgCount = 0;
         let mostTalkativeInRoom;
-        for (let sender in msgsPerChatRoomMap[room]) {
-            if (msgsPerChatRoomMap[room][sender] >= msgCount) {
-                msgCount = msgsPerChatRoomMap[room][sender];
+        for (const sender in msgsPerChatRoom[room]) {
+            if (msgsPerChatRoom[room][sender] >= msgCount) {
+                msgCount = msgsPerChatRoom[room][sender];
                 mostTalkativeInRoom = sender;
             }
         }
@@ -56,17 +51,17 @@ function getChattingStat(input221222) {
     }
 
     // 3. Find the person participating in most chat room
-    const participatingRooms = new Map();
-    for (let chat in input221222) {
-        let chatRoomId = input221222[chat].chatRoomId;
-        let senderId = input221222[chat].senderId;
+    const participatingRooms = {};
+    for (const chat in input) {
+        const chatRoomId = input[chat].chatRoomId;
+        const senderId = input[chat].senderId;
 
         if (!participatingRooms[senderId]) participatingRooms[senderId] = [chatRoomId];
         participatingRooms[senderId].push(chatRoomId);
     }
 
-    const senderAppearanceInEachRoom = new Map();
-    for (let sender in participatingRooms) {
+    const senderAppearanceInEachRoom = {};
+    for (const sender in participatingRooms) {
         senderAppearanceInEachRoom[sender] = new Set(participatingRooms[sender]);
     }
 
